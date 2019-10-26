8f611225120b148d6f6953b139f684622162d745
Ravi Pabari <46424422+RaviPabari@users.noreply.github.com>
2019-10-26 11:58:08 +0530

    This will create 3 nodes and will dispaly them

--- Linked-List/NodeCreation.cpp
+++ Linked-List/NodeCreation.cpp
@@ -1,13 +1,37 @@
 #include<iostream>
 using namespace std;
-struct node{
-	int data;
-	node *next;
+class Node{
+	public:
+		int data;
+		Node *next;
 };
+
+void Display(Node *p){	//traversing ll and printing them
+	while(p!=NULL){
+		cout << p->data <<" " ;
+		p = p->next;
+	}
+}
+
 int main(){
-	node *p;
-	p = new node;
-	p->data =10;
-	p->next=NULL;
+	Node *first=NULL;
+	Node *second=NULL;
+	Node *third=NULL;
+	
+	first = new Node();
+	second = new Node();
+	third = new Node();
+	
+	first -> data = 10;
+	first -> next = second;
+	
+	second -> data = 20;
+	second -> next = third;
+	
+	third -> data = 30;
+	third -> next = NULL;
+	
+	Display(first);
+	
 	return 0;
 }
