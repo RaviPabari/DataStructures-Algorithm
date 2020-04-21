88924628b828a88bc76b65874e2c5cab5c288d34
Ravi Pabari <46424422+RaviPabari@users.noreply.github.com>
2020-04-22 01:07:10 +0530

    Kadane's Algo for maxSubArray

--- Array/Maximum Sub Array.py
+++ Array/Maximum Sub Array.py
@@ -12,14 +12,6 @@ def maxSubarr(arr):
         max_current = max(arr[i], max_current+arr[i] )
         if max_global < max_current:
             max_global = max_current
-    
-    arr.sort()
-    
-    a = b = arr[0]
-    for i in range(1,len(arr)):
-        a = max(arr[i],a+arr[i])
-        if b < a:
-            b = a
     print(str(max_global)+" "+str(b))
 
 
